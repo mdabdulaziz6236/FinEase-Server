bc10b1e0ae3e77e5472c1c19c8e07187a9d04f2d
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-10 13:03:09 +0600

    Added Firebase verify token as middleware and used it

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-service-key.json");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+/* -----------Firebase Token verify---------------- */
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).send({
+      message: "Unauthorized access. Token not found",
+    });
+  }
+  const token = authorization.split(" ")[1];
+  try {
+    await admin.auth().verifyIdToken(token);
+    next();
+  } catch {
+    res.status(401).send({
+      message: "Unauthorized access.",
+    });
+  }
+};
+
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jdeeqhi.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,21 +45,15 @@ async function run() {
   try {
     await client.connect();
 
-    const db = client.db('FinEase')
+    const db = client.db("FinEase");
     const transactionCollection = db.collection("transactions");
 
-
-    app.post('/transactions', async(req,res)=>{
+    app.post("/transactions", verifyToken, async (req, res) => {
       const newTransaction = req.body;
-      const result = await transactionCollection.insertOne(newTransaction)
-      res.send(result)
-    })
+      const result = await transactionCollection.insertOne(newTransaction);
+      res.send(result);
+    });
 
-
-
-
-
-    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
